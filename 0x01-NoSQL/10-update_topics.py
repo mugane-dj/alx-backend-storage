74b23c8c62c76e2e9e73f084f113f76eae1c4fea
mugane-dj <muganedev25@gmail.com>
2023-08-14 18:07:22 +0300

    0xA

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,6 +5,19 @@ update_topics - changes all topics of a school document based on the name
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    The function updates the "topics" field of documents in a MongoDB
+    collection that match a given name.
+
+    :param mongo_collection: The mongo_collection parameter is the
+                             collection in MongoDB where the documents
+                             are stored
+    :param name: The name of the document you want to update in
+                 the MongoDB collection
+    :param topics: The `topics` parameter is a list of topics
+                   that you want to update for a specific document in
+                   the `mongo_collection`
+    """
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
