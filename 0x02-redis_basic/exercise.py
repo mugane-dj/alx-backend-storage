9fb75b4bcb01bf8c2ddc2ae53c398ca4a228ffcf
mugane-dj <muganedev25@gmail.com>
2023-08-16 15:21:52 +0300

    0x0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Cache class - A redis instance
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
