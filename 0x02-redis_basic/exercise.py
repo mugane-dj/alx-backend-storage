789b8f506fac56dbf87c150b4dc4e21f539fa59b
mugane-dj <muganedev25@gmail.com>
2023-08-16 17:31:44 +0300

    0x3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,6 +25,7 @@ def call_history(method: Callable) -> Callable:
         self._redis.rpush(input_key, str(*args))
         output = method(self, *args)
         self._redis.rpush(output_key, output)
+        return output
     return wrapper
 
 
